7e28f1c4413cd0e91ae450141b8110f4e4d34f19
Mr.Aekarat <mr.aekaratz@gmail.com>
2022-07-07 23:48:39 +0700

    css

--- meadow-garden/src/components/layouts/Navbar.js
+++ meadow-garden/src/components/layouts/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './Layout.css'
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
+      <span className='nav-logo'>Aekarat</span>
 
-      
+      <div className='nav-items'>
+          <a href='/home'>Home</a>
+          <a href='/contact'>Contact</a>
+          <a href='/about'>About</a>
+      </div>
     </nav>
   )
 }
